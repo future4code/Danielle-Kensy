efc073b424788897a47d7c6a46fa1542cda432fb
Danielle Kensy <kensydanielle@gmail.com>
2021-11-19 13:37:11 -0300

    Funcionalidade Novas Postagens Adiconada

--- quinzena5/insta4/src/components/Post/Post.js
+++ quinzena5/insta4/src/components/Post/Post.js
@@ -40,12 +40,68 @@ const PostPhoto = styled.img`
   width: 100%;
 `
 
+const Input = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column 
+`
+
 class Post extends React.Component {
   state = {
     curtido: false,
     numeroCurtidas: 0,
     comentando: false,
-    numeroComentarios: 0
+    numeroComentarios: 0,
+    postagens: [
+      {
+        nomeUsuario: 'paulinha',
+        fotoUsuario: 'https://picsum.photos/50/50',
+        fotoPost: 'https://picsum.photos/200/150',
+      },
+      {
+        nomeUsuario: 'Danizinha',
+        fotoUsuario: 'https://picsum.photos/50/51',
+        fotoPost: 'https://picsum.photos/200/151',
+      },
+      {
+        nomeUsuario: 'Mariazinha',
+        fotoUsuario: 'https://picsum.photos/50/52',
+        fotoPost: 'https://picsum.photos/200/152',
+      }
+    ],
+    // váriaveis para input
+    valorInputNome: "",
+    valorInputFoto: "",
+    valorInputPost: "",
+
+  }
+
+  adicionaPost = () => {
+    //Obejto novo post
+    const novoPost = {
+      nomeUsuario: this.state.valorInputNome,
+      fotoUsuario: this.state.valorInputFoto,
+      fotoPost: this.state.valorInputPost
+    }
+
+    const novosPosts = [novoPost, ...this.state.postagens]
+    this.setState({postagens: novosPosts})
+    //LimPando Inputs
+    this.setState({ valorInputNome: "" });
+    this.setState({ valorInputFoto: "" });
+    this.setState({ valorInputPost: "" });
+  }
+  //OnChange para os inputs
+  onChangeNome = (event) => {
+    this.setState({valorInputNome: event.target.value})
+  }
+
+  onChangeFoto = (event) => {
+    this.setState({valorInputFoto: event.target.value})
+  }
+
+  onChangePost = (event) => {
+    this.setState({valorInputPost: event.target.value})
   }
 
   onClickCurtida = () => {
@@ -88,29 +144,57 @@ class Post extends React.Component {
       componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario}/>
     }
 
-    return <PostContainer>
-      <PostHeader>
-        <UserPhoto src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
-        <p>{this.props.nomeUsuario}</p>
-      </PostHeader>
+    const PostagemFeed = this.state.postagens.map((postagem) => {
+      return (
+        <PostContainer>
+          <PostHeader>
+            <UserPhoto src={postagem.fotoUsuario} alt={'Imagem do usuario'}/>
+            <p>{postagem.nomeUsuario}</p>
+          </PostHeader>
 
-      <PostPhoto src={this.props.fotoPost} alt={'Imagem do post'}/>
+          <PostPhoto src={postagem.fotoPost} alt={'Imagem do post'}/>
 
-      <PostFooter>
-        <IconeComContador
-          icone={iconeCurtida}
-          onClickIcone={this.onClickCurtida}
-          valorContador={quantidadeCurtida}
-        />
+          <PostFooter>
+            <IconeComContador
+              icone={iconeCurtida}
+              onClickIcone={this.onClickCurtida}
+              valorContador={quantidadeCurtida}
+            />
 
-        <IconeComContador
-          icone={iconeComentario}
-          onClickIcone={this.onClickComentario}
-          valorContador={this.state.numeroComentarios}
-        />
-      </PostFooter>
-      {componenteComentario}
-    </PostContainer>
+            <IconeComContador
+              icone={iconeComentario}
+              onClickIcone={this.onClickComentario}
+              valorContador={this.state.numeroComentarios}
+            />
+          </PostFooter>
+          {componenteComentario}
+        </PostContainer>
+      )
+    })
+
+    return <div>
+        <Input> 
+          <input
+          value ={this.state.valorInputNome}
+          onChange={this.onChangeNome}
+          placeholder="Nome" 
+          />
+          <input
+          value ={this.state.valorInputFoto}
+          onChange={this.onChangeFoto}
+          placeholder="Link Perfil" 
+          />
+          <input
+          value ={this.state.valorInputPost}
+          onChange={this.onChangePost}
+          placeholder="Link Post" 
+          />
+          <button onClick={this.adicionaPost}>
+            Postar
+          </button>
+        </Input> 
+        {PostagemFeed}
+      </div>
   }
 }
 
